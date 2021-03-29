92ca6ae11fd26b12d97797044f14eabfbfa8b7c4
ovchinnikov.a <ovchinnikov.a@mam.email>
2021-03-29 20:15:59 +0300

    add animation

--- src/components/contactCard/index.js
+++ src/components/contactCard/index.js
@@ -1,14 +1,29 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  LayoutAnimation,
+  UIManager,
+  Platform,
+} from 'react-native';
 import ContactIcon from '../contactIcon';
 import ContactName from '../contactName';
 import ContactInfo from '../contactInfo';
 import userLogo from '../../assets/img/userIcon.png';
 import {contactsStore} from '../data';
 
+if (
+  Platform.OS === 'android' &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
 const ContactCard = () => {
   const [contacts, setContacts] = useState(contactsStore);
-  const cardPress = id =>
+  const cardPress = id => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     setContacts([
       ...contacts.map(item => {
         if (item.id === id) {
@@ -17,10 +32,11 @@ const ContactCard = () => {
         return item;
       }),
     ]);
+  };
 
   return contacts.map(contact => (
     <TouchableOpacity onPress={() => cardPress(contact.id)} key={contact.id}>
-      <View style={{...styles.cardStyle, height: contact.isOpen ? 194 : 90}}>
+      <View style={{...styles.cardStyle, minHeight: contact.isOpen ? 194 : 90}}>
         <View style={styles.topWrapper}>
           <ContactIcon imageUri={userLogo} />
           <View style={styles.infoColumn}>
